0e4e239ef9783d9d3ecbb5b087e4a8082e68f23d
ToberJakub <jakubtober276@gmail.com>
2024-04-18 19:51:52 +0200

    gui creating

--- gui.py
+++ gui.py
@@ -1,6 +1,27 @@
 import tkinter as tk
+from compressmodule import compress, decompress
+
+def compression(i, o):
+    compress(i, o)
 
 window = tk.Tk()
 window.title("Compression engine")
 window.geometry("600x400")
+
+
+input_entry = tk.Entry(window)
+output_entry = tk.Entry(window)
+
+input_label = tk.Label(window, text='File to be compressed')
+output_label = tk.Label(window, text='Name of the compressed file')
+
+compress_button = tk.Button(window, text='Compress')
+
+input_label.grid(row=0,column=0)
+input_entry.grid(row=0,column=1)
+output_label.grid(row=1,column=0)
+output_entry.grid(row=1,column=1)
+compress_button.grid(row=2,column=1)
+
 window.mainloop()
+
